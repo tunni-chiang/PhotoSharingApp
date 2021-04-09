df5a2f55affb5dd93e6592f0e833e1cca880cca9
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-04-09 13:37:59 -0700

    conversion of sample code to hanglebars

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+  res.render('index', {name:"Mark Kim"});
 });
 
 module.exports = router;
