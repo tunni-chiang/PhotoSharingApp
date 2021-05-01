4e5cc40f4a668d487b7682cc01d3fac53589f088
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-05-01 16:44:09 -0700

    lots of work done to registration page; incomplete

--- application/app.js
+++ application/app.js
@@ -6,7 +6,7 @@ var handlebars = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dbRouter = require('./routes/dbtest');
+// var dbRouter = require('./routes/dbtest');
 const { Z_TEXT } = require('zlib');
 
 var app = express();
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/dbtest', dbRouter);
+// app.use('/dbtest', dbRouter);
 app.use('/users', usersRouter);
 
 app.use((err, req, res, next) => {
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../conf/database');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/register', (req, res, next) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let cpassword = req.body.cpassword;
+
+  /**
+   * do server side validation not done in video
+   */
+
+  db.execute("SELECT * FROM users WHERE username=?", [username])
+    .then(([results, fields]) => {
+      if (results && results.length == 0) {
+        return db.execute("SELECT * FROM users WHERE email=?", [email]);
+      } else {
+        throw new UserError(
+          "Registration Failed: Username already exists",
+          "/registration",
+          200
+        );
+      }
+    })
+    .then(([results, fields]) => {
+      if (results && results.length == 0) {
+        let baseSQL = "INSERT INTO users (username, email, password, created) VALUES (?,?,?,now());";
+        return db.execute(baseSQL, [username, email, password]);
+      } else {
+        throw new UserError(
+          "Registration Failed: Email already exists",
+          "/registration",
+          200
+        );
+      }
+    })
+    .then(([results, fields]) => {
+      if(results && results.affectedRows) {
+        successPrint("User.js --> User was created!!");
+        res.redirect('/login');
+      }else{
+        throw new UserError(
+          "Server Error, user could not be created",
+          "/registration",
+          500
+        );
+      }
+    })
+    .catch((err) => {
+      errorPrint("User could not be made", err);
+      if(err instanceof UserError) {
+        errorPrint(err.getMessage());
+        res.status(err.getStatus());
+        res.redirect(err.getRetirectURL());
+      }else{
+        next(err);
+      }
+    });
+})
 
 module.exports = router;
