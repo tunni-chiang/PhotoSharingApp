b05d851df946269906cc87cbfb92c9f11647febb
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-05-05 13:17:54 -0700

    Store post complete; rename image, create and store thumbnail, add database entry

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -18,11 +18,49 @@ var storage = multer.diskStorage({
     }
 });
 
-var uploader = multer({storage: storage});
+var uploader = multer({ storage: storage });
 
 router.post('/createPost', uploader.single("uploadImage"), (req, res, next) => {
-    console.log(req);
-    res.send('');
+    let fileUploaded = req.file.path;
+    let fileAsThumbnail = `thumbnail-${req.file.filename}`;
+    let destinationOfThumbnail = req.file.destination + "/" + fileAsThumbnail;
+    let title = req.body.title;
+    let description = req.body.description;
+    let fk_userId = req.session.userId;
+
+    /** 
+     * do server validation
+     * if any values used for the insert statement are
+     * undefined, mysql.query will fail with the following
+     * error:
+     * BIND parameters cannot be defined
+     */
+
+    sharp(fileUploaded)
+        .resize(200)
+        .toFile(destinationOfThumbnail)
+        .then(() => {
+            let baseSQL = 'INSERT INTO posts (title, description, photopath, thumbnail, created, fk_userId) VALUE (?,?,?,?,now(),?);';
+            return db.execute(baseSQL, [title, description, fileUploaded, destinationOfThumbnail, fk_userId]);
+        })
+        .then(([results, fields]) => {
+            if(results && results.affectedRows) {
+                req.flash('success', "Your post was created successfully!");
+                res.redirect('/');
+            }else{
+                throw new PostError('Post could not be created!', 'postImage', 200);
+            }
+        })
+        .catch((err) => {
+            if(err instanceof PostError){
+                errorPrint(err.getMessage());
+                req.flash('error', err.getMessage());
+                res.status(err.getStatus());
+                res.redirect(err.getRedirectURL());
+            }else{
+                next(err);
+            }
+        })
 });
 
 module.exports = router;
