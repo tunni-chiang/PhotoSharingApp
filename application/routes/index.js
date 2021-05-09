6f112660ae395a98397c9bb7dba57676c1d935f5
mkim797 <mkim797@yahoo.com>
2021-05-08 23:19:56 -0700

    completed viewing individual posts

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postsmiddleware').getRecentPosts;
+var db = require('../config/database');
 
 /* GET home page. */
 router.get('/', getRecentPosts, function (req, res, next) {
@@ -10,25 +11,45 @@ router.get('/', getRecentPosts, function (req, res, next) {
 });
 
 router.get('/login', (req, res, next) => {
-  res.render('login', {title:"Login"});
+  res.render('login', { title: "Login" });
 });
 
 router.get('/registration', (req, res, next) => {
-  res.render('registration', {title:"Registration"});
+  res.render('registration', { title: "Registration" });
 });
 
 router.get('/home', (req, res, next) => {
-  res.render('home', {title:"Home"});
+  res.render('home', { title: "Home" });
 });
 
 router.get('/imagepost', (req, res, next) => {
-  res.render('imagepost', {title:"View Posted Image"});
+  res.render('imagepost', { title: "View Posted Image" });
 });
 
 router.use('/postimage', isLoggedIn);
 
 router.get('/postimage', (req, res, next) => {
-  res.render('postimage', {title:"Post an Image"});
+  res.render('postimage', { title: "Post an Image" });
+});
+
+router.get('/post/:id(\\d+)', (req, res, next) => {
+  let baseSQL = "select u.username, p.title, p.description, p.photopath, p.created \
+  from users u \
+  join posts p \
+  on u.id=fk_userid \
+  where p.id=3;";
+
+  let postId = req.params.id; // variable for server side validation
+  db.execute(baseSQL, [postId])
+    .then(([results, fields]) => {
+      if (results && results.length) {
+        let post = results[0];
+        res.render('imagepost', { currentPost: post });
+      } else {
+        req.flash('error', 'This is not the post you are looking for!');
+        res.redirect('/');
+      }
+    })
 });
 
 module.exports = router;
