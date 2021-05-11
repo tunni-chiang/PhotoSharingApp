a08daa0aaf44bdfe75aecb31331c0be4a3c83f6e
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-05-11 09:17:12 -0700

    small changes before database dump to be added

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -4,8 +4,20 @@ const { successPrint, errorPrint } = require('../helpers/debug/debugprinters');
 const {create} = require('../models/comments');
 
 router.post('/create', (req, res, next) => {
-    console.log(req.body);
-    res.json("we got your comment");
+    // if(req.session.username){
+    //     errorPrint("must be logged in to comment");
+    //     req.json({
+    //         code: -1,
+    //         status: "danger",
+    //         message: "Must be logged in to create comment"
+    //     });
+    // }else{
+
+    // }
+
+    let {comment, postId} = req.body;
+    let username = 'myuser12';
+    let userId = 33
 })
 
 module.exports = router;
