7875fe1a6d3e421288ea18ccd9f7bd65493a1dde
mkim797 <mkim797@yahoo.com>
2021-05-14 11:13:21 -0700

    Added search upon enter

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -23,7 +23,7 @@ var storage = multer.diskStorage({
 var uploader = multer({ storage: storage });
 
 router.post('/createPost', uploader.single("uploadImage"), (req, res, next) => {
-    let fileUploaded = null;// req.file.path;
+    let fileUploaded = req.file.path;
     let fileAsThumbnail = `thumbnail-${req.file.filename}`;
     let destinationOfThumbnail = req.file.destination + "/" + fileAsThumbnail;
     let title = req.body.title;
